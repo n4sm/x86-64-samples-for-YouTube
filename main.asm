7cca2055e4727e7a12230aec4c008f63040b9011
nasm <40870683+feanorlecodeur@users.noreply.github.com>
2019-05-25 19:37:05 +0200

    Add files via upload

---
+++ main.asm
@@ -0,0 +1,53 @@
+; [Assembleur] : 
+;    mips - armv7 - x86 - x86-64 ou x64
+
+; At&t : instruction $0x1, %eax
+; Intel : instruction eax, 0x1
+
+;   [Segment]
+;       .bss = variables non initialisées : int a; == int a = 0, char buf[256];
+;       .data = variables initialisées int a = 1;
+;       .text = code executable : printf("%d", a);
+;
+;   [Registres]
+;       ax -> Eax (Extended ax) -> rax
+;       bx -> Ebx -> rbx
+;       cx -> Ecx -> rcx
+;       dx -> Edx -> rdx
+;       si -> Esi -> rsi # src
+;       di -> Edi -> rsi
+;       sp -> Esp -> rsp
+;       bp -> Ebp -> rbp
+;       ip -> Eip -> rip
+;
+;   [Instructions]
+;       mov met une valeur dans un registre
+;       db déclare un byte (octet)
+;       
+
+
+BITS 64
+
+section .bss
+    a resb 256
+
+section .data
+    hello db `hello world\n`
+    hello_len equ $-hello 
+
+section .text
+    global main ; l'EP ici
+
+main:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, hello
+    mov rdx, hello_len
+    syscall
+    mov rax, 123
+    mov [a], rax
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+
