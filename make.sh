e9dfe30f0f3514b6eead51a64a233a6a669e8c52
nasm <40870683+feanorlecodeur@users.noreply.github.com>
2019-07-02 22:21:18 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -4,3 +4,5 @@ nasm -f elf64 sthack.asm -o sthack.o && ld sthack.o -o sthack
 echo "[*] sthack has been compiled"
 nasm -f elf64 main.asm -o main.o && ld main.o -o main
 echo "[*] main has been compiled"
+nasm -f elf64 condition.asm -o condition.o && ld condition.o -o condition
+echo "[*] condition has been compiled"
