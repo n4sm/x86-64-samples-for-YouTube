c1b3a7e3130c7105be039ff7fd3fc255e10ffc37
nasm <40870683+feanorlecodeur@users.noreply.github.com>
2019-07-03 13:13:37 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -6,3 +6,5 @@ nasm -f elf64 main.asm -o main.o && ld main.o -o main
 echo "[*] main has been compiled"
 nasm -f elf64 condition.asm -o condition.o && ld condition.o -o condition
 echo "[*] condition has been compiled"
+nasm -f elf64 loop.asm -o loop.o && ld loop.o -o loop && ./loop
+echo "[*] loop has been compiled"
